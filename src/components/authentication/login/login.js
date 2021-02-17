c68c3c4ce5aef404d7525fbd9599a960a840c0fb
Michael Barcoe <barcoe98@gmail.com>
2021-02-17 13:10:23 +0000

    updated error handling

--- src/components/authentication/login/login.js
+++ src/components/authentication/login/login.js
@@ -1,8 +1,8 @@
-import React, { useRef, useState }from 'react'
-import { IonContent, IonItem, IonInput, IonList, IonButton, IonIcon, IonLabel, IonAlert} from '@ionic/react';
+import React, { useState }from 'react'
+import { IonContent, IonItem, IonInput, IonList, IonButton, IonIcon, IonLabel, IonLoading, IonText} from '@ionic/react';
 import { logoFacebook, logoGoogle } from 'ionicons/icons';
 import { useAuth } from '../../../contexts/authContext'
-import { Redirect, useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import "../auth.css";
 
 
@@ -11,52 +11,62 @@ const Login = () => {
   const [ email, setEmail ] = useState("")
   const [ pword, setPword ] = useState("")
 
-  const [ loading, setLoading ] = useState(false)
+  const [ status, setStatus ] = useState({loading: false, emailError: false, pwordError: false})
   const history = useHistory();
   const {setLoggedIn} = useAuth();
-  const [showErrorAlert, setShowErrorAlert] = useState(false);
 
   async function handleLogin(e) {
     e.preventDefault()
     try { 
-        setLoading(true)
-        await login(email, pword)
-        setLoggedIn(true)
-        history.push("/")
+      //set loading to true , which displays loading icon
+      //Set errors to false before attempting sign in
+      setStatus({loading: true, emailError: false, pwordError: false})
+      await login(email, pword)
+      //Set loading and errors to false after succesful login
+      setStatus({loading: false, emailError: false, pwordError: false})
+      setLoggedIn(true)
+      history.push("/")
     } 
     catch { 
-        setShowErrorAlert(true)
-        setLoggedIn(false)
+      //Set loading to false after attempted login 
+      //set errors to true and display error message
+      setStatus({loading: false, emailError: true, pwordError: true})
+      setLoggedIn(false)
     }
-    setLoading(false)
-}
+  }
 
+  // <IonAlert
+  //       isOpen={showErrorAlert}
+  //       onDidDismiss={() => setShowErrorAlert(false)}
+  //       backdropDismiss={(true)}
+  //       header={'Input Error'}
+  //       message={'Email and Password is <strong>inccorect</strong>!'}
+  //       buttons={[{
+  //           text: 'Continue',
+  //           handler: () => {console.log('Confirm Continue'); }}]}>
+  //         </IonAlert>
+  
   return (<>
     <IonContent>
       <div id="authContainer">
       <IonList>
         <IonLabel id="pgTitle" >Login</IonLabel>
-        <IonAlert
-        isOpen={showErrorAlert}
-        onDidDismiss={() => setShowErrorAlert(false)}
-        backdropDismiss={(true)}
-        header={'Input Error'}
-        message={'Email and Password is <strong>inccorect</strong>!'}
-        buttons={[{
-            text: 'Continue',
-            handler: () => {console.log('Confirm Continue'); }}]}
-          />
+     
           <IonItem id="rndInput">
             <IonLabel position="stacked">Email</IonLabel>
             <IonInput value={email} type="email" required
             onIonChange={(event) => setEmail(event.detail.value)}></IonInput>
           </IonItem>
+          {status.emailError && <IonText color="danger"> Email Error</IonText> }
           <IonItem id="rndInput">
             <IonLabel position="stacked">Password</IonLabel>
             <IonInput value={pword} type="password" required
             onIonChange={(event) => setPword(event.detail.value)} ></IonInput>
           </IonItem>
-    
+          {status.pwordError && <IonText color="danger"> Password Error</IonText> }
+
+          
+          <IonLoading isOpen={status.loading}></IonLoading>
           <IonButton id="btnTheme" expand="block" color="dark" fill="solid" type="submit" onClick={handleLogin}>
             Login 
           </IonButton>
--- src/components/authentication/signUp/signUp.js
+++ src/components/authentication/signUp/signUp.js
@@ -1,8 +1,8 @@
-import React , { useRef, useState } from 'react'
-import { IonContent, IonItem, IonInput, IonList, IonButton, IonIcon, IonLabel, IonAlert} from '@ionic/react';
+import React , {useState } from 'react'
+import { IonContent, IonLoading, IonItem, IonInput, IonText, IonList, IonButton, IonIcon, IonLabel, IonAlert} from '@ionic/react';
 import {logoFacebook, logoGoogle } from 'ionicons/icons';
 import { useAuth } from '../../../contexts/authContext'
-import { Redirect, useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import "../auth.css";
 
  const Register = () => {
@@ -10,9 +10,8 @@ import "../auth.css";
   const { signUp } = useAuth()
   const [ email, setEmail ] = useState("")
   const [ pword, setPword ] = useState("")
-  const [ confrmPword, setConfrmPword ] = useState("")
-  const [showSignUpError, setShowSignUpError] = useState(false);
-  const [showPwordError, setShowPwordError] = useState(false);
+  //const [ confrmPword, setConfrmPword ] = useState("")
+  const [ status, setStatus ] = useState({loading: false, emailError: false, pwordError: false})
   const history = useHistory()
 
   async function handleSignUp(e) {
@@ -21,12 +20,18 @@ import "../auth.css";
       //   return setShowPwordError(true)
       // }
       try {
+        //set loading to true , which displays loading icon
+        //Set errors to false before attempting sign up
+        setStatus({loading: true, emailError: false, pwordError: false})
         await signUp(email, pword)
-        setShowSignUpError(false)
-        setShowPwordError(false)
+        //Set loading and errors to false after succesful login
+        setStatus({loading: false, emailError: false, pwordError: false})
         history.push("/")
       } catch {
-        setShowSignUpError(true)
+        //Set loading to false after attempted login 
+        //set errors to true and display error message
+        setStatus({loading: false, emailError: true, pwordError: true})
+
       }
     }
 
@@ -35,20 +40,27 @@ import "../auth.css";
       <div id="authContainer">
       <IonList>
         <IonLabel id="pgTitle" >Create an Account</IonLabel>
+
         <IonItem id="rndInput">
           <IonLabel position="stacked">Email</IonLabel>
           <IonInput type="email" id="email" required
           onIonChange={(event) => setEmail(event.detail.value)}></IonInput>
         </IonItem>
+        {status.emailError && <IonText color="danger"> Email Error</IonText> }
+
+
         <IonItem id="rndInput">
           <IonLabel position="stacked">Password</IonLabel>
           <IonInput type="password" id="pword" required
           onIonChange={(event) => setPword(event.detail.value)}></IonInput>
         </IonItem>
+
         <IonItem id="rndInput">
           <IonLabel position="stacked">Confirm Password</IonLabel>
           <IonInput type="password" id="confrmPword" required></IonInput>
         </IonItem>
+        {status.pwordError && <IonText color="danger"> Password Error</IonText> }
+
   
         <IonButton onClick={handleSignUp} id="btnTheme" expand="block" color="dark" shape="" fill="solid" type="submit">
           Register
@@ -70,32 +82,11 @@ import "../auth.css";
 
         <IonButton expand="block" fill="clear" routerLink="/login"> 
           Have an Account? Login</IonButton>
-          
+
+    <IonLoading isOpen={status.loading}></IonLoading>
+
       </IonList>
       </div>
-
-      
-      <IonAlert
-        isOpen={showSignUpError}
-        onDidDismiss={() => setShowSignUpError(false)}
-        backdropDismiss={(true)}
-        header={'Sign Up Error'}
-        message={'Please check your details'}
-        buttons={[{
-            text: 'Continue',
-            handler: () => {console.log('Confirm Continue'); }}]}
-          />
-
-      <IonAlert
-        isOpen={showPwordError}
-        onDidDismiss={() => setShowPwordError(false)}
-        backdropDismiss={(true)}
-        header={'Input Error'}
-        message={'Passwords do not <strong>match</strong>'}
-        buttons={[{
-            text: 'Continue',
-            handler: () => {console.log('Confirm Continue'); }}]}
-          />
   </IonContent>
 
     </>)
