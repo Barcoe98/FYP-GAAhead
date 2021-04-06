c100e6efedc16e36c263dc121929591450bf3c40
Michael Barcoe <barcoe98@gmail.com>
2021-04-06 22:15:29 +0100

    update setup for fitness test like results

--- src/pages/fitnessTest/FitnessTestListPage.js
+++ src/pages/fitnessTest/FitnessTestListPage.js
@@ -2,29 +2,50 @@ import React, { useEffect, useState } from "react";
 import { IonContent, IonPage, IonList } from "@ionic/react";
 import PageHeaderAdd from "../../components/headers/addHeader/index";
 import FitnessTestCard from "../../components/cards/fitnessTestCard/index";
+import AlertError from "../../components/alerts/errorAlert";
 
 import { firestore } from "../../firebase";
-import { useAuth } from "../../contexts/authContext";
 import "../pages.css";
 
 const FitnessTestPage = () => {
   const [fitnessTests, setFitnessTests] = useState([]);
-  const { currentUser } = useAuth();
+  const [managerId, setManagerId] = useState();
+
+  const [errorMessage, setErrorMessage] = useState();
+  const [showAlert, setShowAlert] = useState(false);
+  const myManagerId = '1kK33jibmLZ2RAEb7lF4u9g9STf2'
 
   useEffect(() => {
-    const fTestsRef = firestore
+
+    if (myManagerId !== null ) {
+
+      //set Manager ID to user manager ID
+      setManagerId(myManagerId)
+
+      //ref for user managers fixtures collection
+      const ref = firestore
       .collection("users")
-      .doc(currentUser?.uid)
+      .doc(managerId)
       .collection("fitness_tests");
-    console.log(currentUser?.uid);
-    fTestsRef.get().then((snapshot) => {
-      const tests = snapshot.docs.map((doc) => ({
+
+      //snapshot of doc 
+      ref.get().then((snapshot) => {
+      const docs = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
-      setFitnessTests(tests);
+      setFitnessTests(docs);
     });
-  }, [currentUser]);
+    }
+
+    else {
+      setManagerId(null)
+      setErrorMessage('No Team Data Available, Join a Team')
+      setShowAlert(true)
+      console.log('error') 
+    }
+    
+  }, [managerId]);
 
   return (
     <IonPage>
@@ -35,9 +56,18 @@ const FitnessTestPage = () => {
       <IonContent>
         <IonList id="bg-col">
           {fitnessTests.map((fTest) => (
-            <FitnessTestCard fTest={fTest}></FitnessTestCard>
+            <FitnessTestCard key={fTest.id} fTest={fTest}></FitnessTestCard>
           ))}
         </IonList>
+
+        <AlertError 
+        setShowAlert={() => setShowAlert(false)} 
+        alertHeader='No Fitness Tests Found'
+        showAlert={showAlert} 
+        msg={errorMessage}>
+        </AlertError>
+
+
       </IonContent>
     </IonPage>
   );
