09d0247ad85367f5d0af130eecd38df6450b2642
Michael Barcoe <barcoe98@gmail.com>
2021-04-07 23:17:24 +0100

    added validation to add fitness test results page

--- src/pages/fitnessTest/AddFitnessTestResultsPage.js
+++ src/pages/fitnessTest/AddFitnessTestResultsPage.js
@@ -12,20 +12,45 @@ import {
 } from "@ionic/react";
 import PageHeader from "../../components/headers";
 import TextInputField from "../../components/textInputs/textInputField";
+import AlertError from "../../components/alerts/errorAlert";
+
 import "../pages.css";
 
 const AddFitnessTestResultsPage = () => {
+  
+  const [errorMessage, setErrorMessage] = useState();
+  const [showAlert, setShowAlert] = useState(false);
+
   //TODO add validation
-  const [status, setStatus] = useState({
-    loading: false,
-    emailError: false,
-    pwordError: false,
-  });
   const [time, setTime] = useState("");
+  const [name, setName] = useState("");
+
 
   const handleSaveResults = async () => {
-    //TODO add save results method
-  };
+      const data = {
+        name,
+        time,
+      };
+  
+      if (name === "") {
+        setErrorMessage('No Name Entered')
+        setShowAlert(true)
+      }
+      else if (time === "") {
+        setErrorMessage('No Time Entered')
+        setShowAlert(true)
+      }
+      else {
+        // const ref = firestore
+        // .collection("users")
+        // .doc(currentUser?.uid)
+        // .collection("fitness_tests");
+  
+        // await ref.add(data);
+        // history.goBack();
+        console.log('success')
+      }
+    };
 
   return (
     <IonPage>
@@ -38,6 +63,18 @@ const AddFitnessTestResultsPage = () => {
                 <IonLabel>Fitness Test Details</IonLabel>
               </IonItemDivider>
             </IonCol>
+          </IonRow>
+
+          {/*Name Input Fields & Labels*/}
+          <IonRow>
+            <TextInputField
+              label="Name"
+              text={name}
+              size="12"
+              setText={(e) => setName(e.detail.value)}
+              placeholder="Enter Name"
+              type="text"
+            ></TextInputField>
           </IonRow>
 
           {/*Time Input Fields & Labels*/}
@@ -55,21 +92,28 @@ const AddFitnessTestResultsPage = () => {
           {/* Add Button*/}
           <IonRow>
             <IonCol>
-              <IonLoading isOpen={status.loading}></IonLoading>
               <IonButton
                 onClick={handleSaveResults}
                 id="btnTheme"
                 expand="block"
                 color="dark"
                 fill="solid"
-                type="submit"
-              >
+                type="submit">
                 Save Results
               </IonButton>
+
             </IonCol>
           </IonRow>
         </IonGrid>
       </IonContent>
+
+      <AlertError 
+        setShowAlert={() => setShowAlert(false)} 
+        alertHeader='Please Fill All Required Fields'
+        showAlert={showAlert} 
+        msg={errorMessage}>
+      </AlertError>
+
     </IonPage>
   );
 };
