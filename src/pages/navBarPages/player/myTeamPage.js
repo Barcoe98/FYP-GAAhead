7ae44de5ca9d7345122db56cd3bdb54bae1eb5ce
Michael Barcoe <barcoe98@gmail.com>
2021-04-26 22:29:33 +0100

    updated nav page images

--- src/pages/navBarPages/player/myTeamPage.js
+++ src/pages/navBarPages/player/myTeamPage.js
@@ -4,61 +4,47 @@ import TitleHeader from "../../../components/headers/titeHeader";
 
 import "../../pages.css";
 
-
 const MyTeamPage = () => {
   
   return (
-    <IonPage>
-      <IonContent id="bg-col">
-        <TitleHeader title="My Team"></TitleHeader>
+  <IonPage>
+    <IonContent id="bg-col">
+      <TitleHeader title="My Team"></TitleHeader>
         <IonGrid>
+
           <IonRow>
-            <IonCol id="navBtnYlw" size="6">
-              <ion-router-link
-                id="navLnk"
-                href="/trainingschedule/list"
-              >
-                Training Schedules
+            <IonCol id="navFixtureBtn" size="6">
+              <ion-router-link id="navLnk" href="/fixture/list">Fixtures</ion-router-link>
+            </IonCol>
+            <IonCol id="navResultsRed" size="6">
+              <ion-router-link id="navLnk" href="/result/list">Results</ion-router-link>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol id="navTeamBtn" size="12">
+              <ion-router-link id="navLnk" href="/team/panel/list">Team Players</ion-router-link>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol id="navTrainingBtn" size="6">
+              <ion-router-link id="navLnk" href="/trainingschedule/list">Training Schedules</ion-router-link>
+            </IonCol>
+            <IonCol id="navWorkoutsBtn" size="6">
+              <ion-router-link id="navLnk" href="/workout/list">Workout Routines
               </ion-router-link>
             </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol id="navFTestsBtn" size="12">
+              <ion-router-link id="navLnk" href="/fitness/test/list">Fitness Tests</ion-router-link>
+            </IonCol>
+          </IonRow>
+
           
-            <IonCol id="navBtnGrn" size="6">
-              <ion-router-link id="navLnk" href="/workout/list">
-                Workout Routines
-              </ion-router-link>
-            </IonCol>
-          </IonRow>
-
-          <IonRow>
-            <IonCol id="navBtnRed" size="12">
-              <ion-router-link id="navLnk" href="/fitness/test/list">
-                Fitness Tests
-              </ion-router-link>
-            </IonCol>
-          </IonRow>
-
-
-          <IonRow>
-            <IonCol id="navBtnBlue" size="6">
-              <ion-router-link id="navLnk" href="/fixture/list">
-                Fixtures
-              </ion-router-link>
-            </IonCol>
-
-            <IonCol id="navBtnPrpl" size="6">
-              <ion-router-link id="navLnk" href="/result/list">
-                Results
-              </ion-router-link>
-            </IonCol>
-          </IonRow>
-
-          <IonRow>
-            <IonCol id="navBtnTeal" size="12">
-              <ion-router-link id="navLnk" href="/team/panel/list">
-                Team Players
-              </ion-router-link>
-            </IonCol>
-          </IonRow>
+         
 
         </IonGrid>
       </IonContent>
