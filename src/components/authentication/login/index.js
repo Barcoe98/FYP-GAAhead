3085b658f8d359e3eb8c6caa53c1c63a7423e78e
Michael Barcoe <barcoe98@gmail.com>
2021-05-02 23:25:49 +0100

    finihsed app seperation code

--- src/components/authentication/login/index.js
+++ src/components/authentication/login/index.js
@@ -1,9 +1,29 @@
 import React, { useState } from "react";
-import {  IonContent, IonItem,  IonInput,  IonList, IonButton, IonLabel } from "@ionic/react";
-import AlertError from "../../../components/alerts/errorAlert";
-
+import { IonApp, IonRouterOutlet} from '@ionic/react';
+import ReactDOM from 'react-dom';
+import {
+  IonContent,
+  IonItem,
+  IonInput,
+  IonList,
+  IonButton,
+  IonIcon,
+  IonLabel,
+  IonLoading,
+  IonText,
+} from "@ionic/react";
 import { useAuth } from "../../../contexts/authContext";
 import { useHistory } from "react-router-dom";
+import {Route , Redirect} from 'react-router-dom'
+import {IonReactRouter } from '@ionic/react-router'
+import ManagerTabs from '../../tabs/manager/index'
+import PlayerTabs from '../../tabs/player/index'
+import SignUpPage from '../signUp/index';
+import LoginPage from '../login/index';
+import ForgotPasswordPage from '..//forgotPassword/index';
+import {firestore} from'../../../firebase'
+
+
 import "../auth.css";
 
 const Login = () => {
@@ -11,47 +31,132 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [pword, setPword] = useState("");
 
-  const [errorMessage, setErrorMessage] = useState();
-  const [showAlert, setShowAlert] = useState(false);
-
+  const [status, setStatus] = useState({
+    loading: false,
+    emailError: false,
+    pwordError: false,
+  });
   const history = useHistory();
-  const { setLoggedIn } = useAuth();
+
+  const { setLoggedIn, currentUser } = useAuth();
+  var [userType, setUserType] = useState();
+
+  function ManagerNavBar() {
+    return (
+      <IonApp>
+        <IonReactRouter>       
+          <IonRouterOutlet>
+            <Route exact path="/signup"><SignUpPage></SignUpPage></Route>
+            <Route exact path="/login"><LoginPage></LoginPage></Route>
+            <Route exact path="/forgotpassword"><ForgotPasswordPage></ForgotPasswordPage></Route>
+            <ManagerTabs></ManagerTabs>
+            <Redirect exact path="/" to ="/login"></Redirect>
+          </IonRouterOutlet>
+        </IonReactRouter>
+    </IonApp>
+    )
+  }
+
+  function PlayerNavBar() {
+    return (
+      <IonApp>
+        <IonReactRouter>       
+          <IonRouterOutlet>
+            <Route exact path="/signup"><SignUpPage></SignUpPage></Route>
+            <Route exact path="/login"><LoginPage></LoginPage></Route>
+            <Route exact path="/forgotpassword"><ForgotPasswordPage></ForgotPasswordPage></Route>
+            <PlayerTabs></PlayerTabs>
+            <Redirect exact path="/" to ="/login"></Redirect>
+          </IonRouterOutlet>
+        </IonReactRouter>
+    </IonApp>
+    )
+  }
+
 
   async function handleLogin(e) {
     e.preventDefault();
     try {
       //set loading to true , which displays loading icon
       //Set errors to false before attempting sign in
+      setStatus({ loading: true, emailError: false, pwordError: false });
       await login(email, pword);
+      
       setLoggedIn(true);
-      history.push("/more");
+      const ref = firestore
+      .collection("users")
+      .doc(currentUser?.uid)
+  
+    ref.get(currentUser?.uid).then(doc => {
+      if (!doc.exists) {
+        console.log('No such document');
+        //history.goBack();
+      } else {
+        const userDoc = { id: doc.id, ...doc.data() };  
+
+        if (userDoc?.userType === "manager") {
+          return ( 
+            ReactDOM.render(<ManagerNavBar/>, document.getElementById("root")),
+            history.push("/more")
+          )
+        }
+        else {
+          return ( 
+            ReactDOM.render(<PlayerNavBar/>, document.getElementById("root")),
+            history.push("/more")
+          )
+        }
+      }
+    });
+      
     } catch {
       //Set loading to false after attempted login
       //set errors to true and display error message
-      setErrorMessage('Please Try Again')
-      setShowAlert(true)
-      setLoggedIn(false);
+      setStatus({ loading: false, emailError: true, pwordError: true });
+      //setLoggedIn(false);
     }
   }
 
   return (
+    <>
       <IonContent>
         <div id="authContainer">
           <IonList id="pg-col">
-
             <IonLabel id="pgTitle">Login</IonLabel>
-
             <IonItem color="light" id="rndInput">
               <IonLabel position="stacked">Email</IonLabel>
-              <IonInput value={email} type="email" required onIonChange={(event) => setEmail(event.detail.value)}></IonInput>
+              <IonInput
+                value={email}
+                type="email"
+                required
+                onIonChange={(event) => setEmail(event.detail.value)}
+              ></IonInput>
             </IonItem>
-
-            <IonItem lines="true" color="light" id="rndInput">
+            {status.emailError && (
+              <IonText color="danger"> Email Error</IonText>
+            )}
+            <IonItem color="light" id="rndInput">
               <IonLabel position="stacked">Password</IonLabel>
-              <IonInput value={pword} type="password" required onIonChange={(event) => setPword(event.detail.value)}></IonInput>
+              <IonInput
+                value={pword}
+                type="password"
+                required
+                onIonChange={(event) => setPword(event.detail.value)}
+              ></IonInput>
             </IonItem>
-    
-            <IonButton id="btnTheme" expand="block" color="dark" fill="solid" type="submit" onClick={handleLogin}>
+            {status.pwordError && (
+              <IonText color="danger"> Password Error</IonText>
+            )}
+
+            <IonLoading isOpen={status.loading}></IonLoading>
+            <IonButton
+              id="btnTheme"
+              expand="block"
+              color="light"
+              fill="solid"
+              type="submit"
+              onClick={handleLogin}
+            >
               Login
             </IonButton>
 
@@ -62,20 +167,10 @@ const Login = () => {
             <IonButton expand="block" fill="clear" routerLink="/signup">
               Need an Account ? Sign Up
             </IonButton>
-
           </IonList>
         </div>
-
-        <AlertError 
-          setShowAlert={() => setShowAlert(false)} 
-          alertHeader='Login Details Incorrect'
-          showAlert={showAlert} 
-          msg={errorMessage}>
-        </AlertError>
-
       </IonContent>
-
-      
+    </>
   );
 };
 export default Login;
--- src/index.js
+++ src/index.js
@@ -18,53 +18,13 @@ import './globals/variables.css';
 import './globals/fonts/GothamRounded-Medium.ttf'
 
 import { IonApp, IonRouterOutlet} from '@ionic/react';
-import ManagerTabs from './components/tabs/manager/index'
-import PlayerTabs from './components/tabs/player/index'
 import SignUpPage from './components/authentication/signUp/index';
 import LoginPage from './components/authentication/login/index';
 import ForgotPasswordPage from './components/authentication/forgotPassword/index';
 import AuthContextProvider from "./contexts/authContext"
 import {IonReactRouter } from '@ionic/react-router'
-import {Route , Redirect} from 'react-router-dom'
+import {Route, Redirect} from 'react-router-dom'
 
-import { useAuth} from '././contexts/authContext'
-import {firestore} from'./firebase'
-
-function ManagerNavBar() {
-  return (
-    <IonApp>
-    <AuthContextProvider>
-      <IonReactRouter>       
-        <IonRouterOutlet>
-          <Route exact path="/signup"><SignUpPage></SignUpPage></Route>
-          <Route exact path="/login"><LoginPage></LoginPage></Route>
-          <Route exact path="/forgotpassword"><ForgotPasswordPage></ForgotPasswordPage></Route>
-          <ManagerTabs></ManagerTabs>
-          <Redirect exact path="/" to ="/login"></Redirect>
-        </IonRouterOutlet>
-      </IonReactRouter>
-    </AuthContextProvider>
-  </IonApp>
-  )
-}
-
-function PlayerNavBar() {
-  return (
-    <IonApp>
-    <AuthContextProvider>
-      <IonReactRouter>       
-        <IonRouterOutlet>
-          <Route exact path="/signup"><SignUpPage></SignUpPage></Route>
-          <Route exact path="/login"><LoginPage></LoginPage></Route>
-          <Route exact path="/forgotpassword"><ForgotPasswordPage></ForgotPasswordPage></Route>
-          <PlayerTabs></PlayerTabs>
-          <Redirect exact path="/" to ="/login"></Redirect>
-        </IonRouterOutlet>
-      </IonReactRouter>
-    </AuthContextProvider>
-  </IonApp>
-  )
-}
 
 function NavBar() {
   return (
@@ -74,6 +34,7 @@ function NavBar() {
         <IonRouterOutlet>
           <Route exact path="/signup"><SignUpPage></SignUpPage></Route>
           <Route exact path="/login"><LoginPage></LoginPage></Route>
+          <Route exact path="/forgotpassword"><ForgotPasswordPage></ForgotPasswordPage></Route>
           <LoginPage></LoginPage>
           <Redirect exact path="/" to ="/login"></Redirect>
         </IonRouterOutlet>
@@ -83,55 +44,5 @@ function NavBar() {
   )
 }
 
-function IsLoggedInFunc(props) {
-  const { currentUser } = useAuth()
-
-  if (currentUser?.uid === null) {
-    console.log('loggedIn')
-    return <UserNavBar/>;
-  }
-  else {
-    console.log('norm nav')
-    return <NavBar/>;
-  }
-}
-
-
-function UserNavBar() {
-  const { currentUser } = useAuth()
-  var [userType, setUserType] = useState();
-
-  const ref = firestore
-    .collection("users")
-    .doc('YEBa5ttffIbKzRY9VLwj8NebIPI2')
-
-  ref.get('YEBa5ttffIbKzRY9VLwj8NebIPI2').then(doc => {
-    if (!doc.exists) {
-      console.log('No such document');
-      //history.goBack();
-    } else {
-      const userDoc = { id: doc.id, ...doc.data() };
-
-      //set ManagerId Attributes to matching in DB
-      setUserType(userDoc?.userType)
-
-      console.log(userDoc?.userType)
-    }
-  });
-
-  if (userType === "manager") {
-    console.log(userType)
-    return <ManagerNavBar/>;
-  }
-  else if (userType === "player") {
-    console.log(userType)
-    return <PlayerNavBar/>;
-  }
-  else {
-    return <NavBar/>;
-  }
-  
-}
-
-ReactDOM.render(<PlayerNavBar />, document.getElementById("root"));
+ReactDOM.render(<NavBar />, document.getElementById("root"));
 
